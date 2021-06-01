101e8a9dae92d06c5059ddd3f312ccf59979d4fa
seyoung.sim <ssiox3@gmail.com>
2021-06-01 18:13:20 +0900

    주석 추가 및 gitignore 업데이트

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,9 @@
+/**
+ * tailwind 를 일반 CSS파일로 빌드하기 위해 사용하는 프레임워크.
+ * JS 플러그인을 사용하여 CSS를 변환해준다. 파이프.
+ * 플러그인으로는 tailwindcss, autoprefixer를 추가한다.
+ * autopefixer: 구형 브라우저에서도 호환이 될 수 있도록 css 프로퍼티에 접두사(prefix)를 붙여준다.
+ */
 module.exports = {
   plugins: {
     tailwindcss: {},
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+/**
+ * tailwind를 확장할 수 있도록 해주는 config 파일.
+ * tailwind css를 커스텀 가능하다.
+ */
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
@@ -8,4 +12,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
