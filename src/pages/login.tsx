9bb9b9a7fa403cb62e43f51a645dd50016ec548b
seyoung.sim <ssiox3@gmail.com>
2021-06-02 18:38:28 +0900

    tailwind CSS로 login form 컴포넌트 디자인 .

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,27 @@
 /**
- * 로그인 컴포넌트
+ * 로그인 컴포넌트. tailwind로 css 디자인.
  */
 import React from "react";
 
 export const Login = () => {
-  return <span>Login</span>;
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-800">
+      <div className="bg-white w-full max-w-lg py-10 rounded-lg text-center">
+        <h3 className="text-3xl text-gray-800">Log In</h3>
+        <form className="flex flex-col mt-5 px-5">
+          <input
+            placeholder="Email"
+            className=" bg-gray-100 shadow-inner border-2 focus:border-opacity-60 focus:border-green-600 focus:outline-none mb-3 py-3 px-5 rounded-lg"
+          />
+          <input
+            placeholder="Password"
+            className=" bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-60 focus:border-green-600  py-3 px-5 rounded-lg"
+          />
+          <button className="py-3 px-5 bg-gray-800 text-white mt-3 text-lg rounded-lg focus:outline-none hover:opacity-90">
+            Log In
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
