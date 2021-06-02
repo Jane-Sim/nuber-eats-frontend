9838947dbfe09706fb2aebf5b8cce20658613566
seyoung.sim <ssiox3@gmail.com>
2021-06-02 10:57:05 +0900

    react-router-dom 설치 및 router 폴더 생성.

---
+++ src/routers/logged-in-router.tsx
@@ -0,0 +1,6 @@
+/**
+ * 사용자가 로그인 상태일 때 보여줄 Router
+ */
+import React from "react";
+
+export const LoggedInRouter = () => <span>Logged In</span>;
---
+++ src/routers/logged-out-router.tsx
@@ -0,0 +1,6 @@
+/**
+ * 사용자가 로그아웃 상태일 때 보여줄 Router
+ */
+import React from "react";
+
+export const LoggedOutRouter = () => <span>Logged Out</span>;
