4a1d81132e8d9c1269ce65ef1223b02c0f73f2c4
seyoung.sim <ssiox3@gmail.com>
2021-06-08 16:41:44 +0900

    모든 백엔드 요청을 보낼 때, header에 토큰을 지정하여 넣는 로직 추가. (apollo Link 사용으로 authentication 작업 추가)

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,7 +1,13 @@
 /**
  * 백엔드의 graphql 서버와 연동되는 apollo client 모듈 생성
  */
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { LOCALSTORAGE_TOKEN } from "./constants";
 
 const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
@@ -14,10 +20,29 @@ const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
 export const isLoggedInVar = makeVar(Boolean(token));
 export const authTokenVar = makeVar(token);
 
+// authentication 작업을 위해, Apollo Link를 생성한다. http, auth, web sockets 링크를 가질 예정.
+// 1. 첫 작업인 link chain을 위해, 서버의 HTTP URL을 HttpLink로 생성한다.
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000/graphql",
+});
+
+// 2. 또다른 Apollo Link인 authLink는, graphql 요청시 header에 토큰을 추가하여 보낼 수 있도록, setContext 를 사용한다.
+// setContext는 클라이언트가 만든 requset context를 set 하는 기능이다.
+// 기존의 header context를 return 후, token context를 추가해준다.
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      "x-jwt": authTokenVar() || "",
+    },
+  };
+});
+
+// apollo link를 통해, authentication 작업을 할 수 있다.
 // InMemoryCache를 이용해, 특정 쿼리의 key-value값을 저장한다.
 // reactive variabled한 field를 참고하기에, 해당 쿼리는 자동으로 새로고침된다.
 export const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,12 +1,36 @@
 /**
  * 사용자가 로그인 상태일 때 보여줄 Router
  */
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
 import React from "react";
-import { isLoggedInVar } from "../apollo";
+import { meQuery } from "../__generated__/meQuery";
 
-export const LoggedInRouter = () => (
-  <div>
-    <h1>Logged In</h1>
-    <button onClick={() => isLoggedInVar(false)}>Log Out</button>
-  </div>
-);
+// 백엔드에 me query를 날릴 때 사용하는 graphql ME_QUERY 객체
+const ME_QUERY = gql`
+  query meQuery {
+    me {
+      id
+      email
+      role
+      verified
+    }
+  }
+`;
+
+export const LoggedInRouter = () => {
+  const { data, loading, error } = useQuery<meQuery>(ME_QUERY);
+  // 데이터가 없거나 로딩중이거나 에러가 있으면 아래의 컴포넌트를 반환한다.
+  if (!data || loading || error) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <span className="font-medium text-xl tracking-widest">Loading...</span>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>{data.me.email}</h1>
+    </div>
+  );
+};
