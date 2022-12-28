4a2c4b02e474ea4a81db85bf52957137f676f6e1
seyoung-VIRNECT <jane.sinm@virnect.com>
2022-12-28 21:01:39 +0900

    feat: cypress e2e test - verify localStorage token after login

--- cypress/e2e/first-test.cy.ts
+++ cypress/e2e/first-test.cy.ts
@@ -7,19 +7,31 @@
 
 
 describe("Log In", () => {
+  const user = cy;
   it("should see login page", () => {
       // cy는 cypress의 줄임말.
-    cy.visit("/").title().should("eq", "Login | Nuber Eats");
+    user.visit("/").title().should("eq", "Login | Nuber Eats");
   })
-  it("can fill out the form", () => {
-    cy.visit("/");
-    cy.findByPlaceholderText(/email/i).type("owner@naver.com");
-    cy.findByPlaceholderText(/password/i).type("12345");
-    cy.findByRole("cmbutton").should("not.have.class", "pointer-events-none");
-  });
   it("can see email / password validation errors", () => {
-    cy.visit("/");
-    cy.findByPlaceholderText(/email/i).type("bad@email")
-    cy.findByRole("alert").should("have.text", "Please enter a valid email");
+    user.visit("/");
+    user.findByPlaceholderText(/email/i).type("bad@email")
+    user.findByRole("alert").should("have.text", "Please enter a valid email");
+    user.findByPlaceholderText(/email/i).clear();
+    user.findByRole("alert").should("have.text", "Email is required");
+    user.findByPlaceholderText(/email/i).type("bad@email.com");
+    user
+    .findByPlaceholderText(/password/i).type("a").clear();
+    user.findByRole("alert").should("have.text", "Password is required");
+  });
+  it("can fill out the form", () => {
+    user.visit("/");
+    user.findByPlaceholderText(/email/i).type("owner@naver.com");
+    user.findByPlaceholderText(/password/i).type("12345");
+    user.findByRole("cmbutton").should("not.have.class", "pointer-events-none").click();
+    // user.window().its 는 window의 property.
+    user.window().its("localStorage.nuber-token").should("be.a", "string");
+  });
+  it("sign up", () => {
+    user.visit("/create-account");
   });
 })
